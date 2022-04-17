fbdc799d47d386f31df3e6fe5d2b954f79fae5d6
EstebanAriasLima <estebanariaslima@gmail.com>
2022-04-17 23:12:33 +0000

    se empuja el modelo de datos de instagram

--- src/models.py
+++ src/models.py
@@ -8,26 +8,81 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+#     def to_dict(self):
+#         return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key = True)
+    username = Column(String(50), nullable = False)
+    email = Column(String(100), nullable = False)
+    biography = Column (String(120), nullable = True)
+    post = relationship('Post', backref='user')
+    reel = relationship('Reel', backref='user')
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    photo = Column(String(120), nullable = True)
+    video = Column(String(120), nullable = True)
+
+class Reel(Base):
+    __tablename__ = 'reel'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    video = Column(String(120), nullable = False)
+
+class Story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    photo = Column(String(120), nullable = True)
+    video = Column(String(120), nullable = True)
+    song = Column(String(120), nullable = True)
+    text = Column(String(120), nullable = True)
+
+class comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key = True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    reel_id = Column(Integer, ForeignKey('reel.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    story_id = Column(Integer, ForeignKey('story.id'))
+    story = relationship("Story")
+    post = relationship("Post")
+    reel = relationship("Reel")
+    user = relationship("User")
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key = True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    reel_id = Column(Integer, ForeignKey('reel.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    story_id = Column(Integer, ForeignKey('story.id'))
+    story = relationship("Story")
+    reel = relationship("Reel")
+    post = relationship("Post")
+    user = relationship("User")
 
 ## Draw from SQLAlchemy base
 try:
